851c1ea856a9bc4ba8339c76044a936dfb5babf2
Xuexiang Xu <xuexu@microsoft.com>
2023-08-26 15:46:36 +0800

    gesture

--- EmojiArt/UtilityExtensions.swift
+++ EmojiArt/UtilityExtensions.swift
@@ -45,6 +45,28 @@ extension CGPoint {
     }
 }
 
+extension CGSize {
+    var center: CGPoint {
+        CGPoint(x: width / 2, y: height / 2)
+    }
+    
+    static func +(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+    
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+    }
+    
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+    }
+}
+
 extension Character {
     var isEmoji: Bool {
         if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
