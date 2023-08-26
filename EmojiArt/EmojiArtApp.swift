c44eb4257f4d9ae8eab104addd88c85e491ce6f3
Xuexiang Xu <xuexu@microsoft.com>
2023-08-26 10:07:39 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Xuexiang Xu on 2023/8/26.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
